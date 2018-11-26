a70a3433f685ab6f94044ca128cd37b5ec19c261
cedricfrancoys <cedricfrancoys@gmail.com>
2018-11-26 13:29:00 +0100

    added mkdocs

---
+++ docker/accounts/mkdocs/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -f ../.env ]
+then
+    mkdir /home/$USERNAME/doc
+    
+    mkdir /home/$USERNAME/doc/mkdocs
+    mkdir /home/$USERNAME/doc/mkdocs/docs
+
+    cp /home/$USERNAME/mkdocs.yml /home/$USERNAME/doc/mkdocs/mkdocs.yml
+    
+    cd /home/$USERNAME/doc    
+    
+    ln -s ./mkdocs/docs docs
+    ln -s ./mkdocs/mkdocs.yml mkdocs.yml
+
+    # assign ownership to user and www-data (group)
+    chown -R $USERNAME:www-data /home/$USERNAME/doc    
+    
+else
+    echo ".env file is missing"
+fi
+
---
+++ docker/accounts/odoo/init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This script has t obe run after general user initialisation: init.sh
+
+#!/bin/bash
+
+if [ -f ../.env ]
+then
+
+    # Create a odoo user (to link host with VM), no home, no login, no prompt
+    adduser --no-create-home --disabled-login --gecos "" nabu
+
+    # Build docked-odoo image
+    cd /home/docker/images/docked-odoo/
+    /home/docker/images/docked-odoo/build.sh
+
+    # Edit account parameters and then Run script for account creation
+    cd /home/docker/accounts
+    vi .env ; /home/docker/accounts/init.sh
+
+
+    cd /home/$USERNAME
+
+    echo "retrieving addons from github repositories"
+    git config --global credential.helper cache
+    git clone https://github.com/odoo/enterprise --depth 1 --branch 12.0 --single-branch ./odoo/enterprise
+    git clone https://github.com/ARTECOM/odoo-external.git ./odoo/external
+    git clone https://github.com/ARTECOM/odoo-artecom.git ./odoo/custom
+
+    chown odoo:odoo -R /home/$USERNAME/odoo
+else
+    echo ".env file is missing"
+fi
+    
